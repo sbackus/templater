5ef5b35d3a3bea00582a29899555db1ceb272df1
Sam Backus <Samuel.Backus@gmail.com>
2014-10-29 22:23:46 -0400

    add single substitution

--- spec/templater_spec.rb
+++ spec/templater_spec.rb
@@ -3,6 +3,7 @@ require './templater.rb'
 
 # TODO:
 # key value substitution
+# multiple key value substitution
 # loop over lists of values
 
 # Questions:
@@ -15,10 +16,22 @@ require './templater.rb'
 
 describe "templater" do
 	it "returns the template without modifications if it contains no tags" do
-		template = "<h1> Pure HTML </h1> \n <p> This has no template tags </p>"
+		template = "<h1> Pure HTML </h1> \n <p> This has no template tags! </p>"
 		expected_result = template
 		expect(templater(template, nil)).to eq expected_result
 	end
+	it "substitutes a template tag with data" do
+		template = "<h1> <* title *> </h1>"
+		data = JSON.parse('{"title": "substitution works!"}')
+		expected_result = "<h1> substitution works! </h1>"
+		expect(templater(template, data)).to eq expected_result
+	end
+	# it "substitutes multiple template tags with data" do
+	# 	template = "<h1> <*title*> </h1> <p> <*body*> </p>"
+	# 	data = JSON.parse('{"title": "substitution works!", "body": "for the body too!"}')
+	# 	expected_result = "<h1> substitution works! </h1> <p> for the body too! </p>"
+	# 	expect(templater(template, data)).to eq expected_result
+	# end
 end
 
 
--- templater.rb
+++ templater.rb
@@ -1,5 +1,11 @@
-
+require 'json'
 
 def templater(template, data)
+
+	tags = template.scan(/<\*.*\*>/)
+ 	for tag in tags
+ 		replacement = data[tag[2..-3].strip]
+ 		template.gsub!(tag, replacement)
+	end
 	return template
 end
