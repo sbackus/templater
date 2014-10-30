164ebe0953d3733fe8c567885ca6bb535f94101b
Sam Backus <Samuel.Backus@gmail.com>
2014-10-29 23:17:35 -0400

    refactor

--- templater.rb
+++ templater.rb
@@ -4,7 +4,7 @@ def templater(template, json)
 
 	tags = template.scan(/<\*.*?\*>/)
  	for tag in tags
- 		replacement = get_data(json, tag[2..-3].strip)
+ 		replacement = get_data(json, tag[2..-3].strip.split("."))
  		template.gsub!(tag, replacement)
 	end
 	return template
@@ -12,11 +12,10 @@ end
 
 
 def get_data(json, path_to_data)
-	names = path_to_data.split(".")
-	first = names.first
-	if names.length == 1
+	first = path_to_data.first
+	if path_to_data.length == 1
 		return json[first]
 	else
-		return get_data(json[first], names[1..-1].join("."))
+		return get_data(json[first], path_to_data[1..-1])
 	end
 end
