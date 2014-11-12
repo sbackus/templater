8e2f45de867118f362eb221c17f7ac0dd0edab14
Sam Backus <Samuel.Backus@gmail.com>
2014-11-11 22:29:12 -0500

    refactor so that the templates get compiled into an abstract syntax trees before rendering them so that it will be easier to add EACH.

--- templater.rb
+++ templater.rb
@@ -4,20 +4,72 @@ class Templater
 
   def initialize(template)
     @template = template
+    @root = Compiler.new(template).compile()
   end
 
 	def render(json)
-		tag_regex = /<\*.*?\*>/
-		tags = @template.scan(tag_regex)
-	 	for tag in tags
-	 		replacement = get_data(json, tag[2..-3].strip.split("."))
-	 		@template.gsub!(tag, replacement)
-		end
-		return @template
+		@root.render(json)
 	end
 end
 
+class Compiler
+	TAG_START = '<\*'
+	TAG_END = '\*>'
+	TAG_REGEX = /(#{TAG_START}.*?#{TAG_END})/
+	def initialize(template)
+		@template = template
+	end
 
+	def get_tokens()
+		@template.split(TAG_REGEX)
+	end
+
+	def compile
+    root_node = Node.new(@template)
+    get_tokens().each do |token|
+        new_node = create_node(token)
+        root_node.children << new_node
+    end
+    return root_node
+	end
+
+	def create_node(token)
+		if token =~ TAG_REGEX
+			return VariableNode.new(token[2...-2].strip)
+		else
+			return TextNode.new(token)
+		end
+	end
+
+end
+
+class Node
+
+	attr_accessor :children
+
+	def initialize(token)
+		@token = token
+		@children = []
+	end
+
+	def render(context)
+		@children.map do |child_node|
+			child_node.render(context)
+		end.join
+	end
+end
+
+class VariableNode < Node
+	def render(context)
+		get_data(context, @token.split("."))
+	end
+end
+
+class TextNode < Node
+	def render(context)
+		return @token
+	end
+end
 
 def get_data(json, path_to_data)
 	first = path_to_data.first
