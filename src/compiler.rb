aadf86abf94a0871e2a8857aaa93aeb8a78a0c18
Sam Backus <Samuel.Backus@gmail.com>
2015-07-26 16:54:13 -0400

    small refactoring

--- src/compiler.rb
+++ src/compiler.rb
@@ -1,33 +1,25 @@
 class Compiler
 
-  TAG_START = '<\*'
-  TAG_END = '\*>'
-  TAG = /(#{TAG_START}.*?#{TAG_END})/
-  END_EACH = /(#{TAG_START}\WENDEACH\W#{TAG_END})/
-  START_EACH = /(#{TAG_START}\WEACH.*?#{TAG_END})/
-
   def self.compile(tokens)
     root_node = RootNode.new(nil)
     context_stack = [root_node]
     tokens.each do |token|
       if token.new_context?
-        new_node = add_child_to_tree(token, context_stack)
+        new_node = token.create_node
+        add_child_to_tree(new_node, context_stack.last)
         context_stack.push(new_node)
       elsif token.end_context?
         context_stack.pop
       else
-        add_child_to_tree token, context_stack
+        add_child_to_tree(token.create_node, context_stack.last)
       end
     end
     return root_node
   end
 
 private
-  def self.add_child_to_tree(token, context_stack)
-    new_node = token.create_node
-    context_node = context_stack.last
-    context_node.children << new_node
-    new_node
+  def self.add_child_to_tree(new_node, parent_node)
+    parent_node.children << new_node
   end
 
 end
